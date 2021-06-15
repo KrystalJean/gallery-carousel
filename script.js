cf25ff242f97e128db449d14b4ac03b29e1024e8
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2021-06-15 18:20:14 -0500

    Init commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000) //in milliseconds
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length -1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)` //adjust 2 times in CSS, 1 time in JS, (px size should match container and img width).
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
+
